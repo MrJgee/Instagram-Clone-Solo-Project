b92af95c4554f988382f5540b35880a577961b69
MrJgee <178556824+MrJgee@users.noreply.github.com>
2026-02-21 19:05:30 +1030

    Files-updated

---
+++ index.js
@@ -0,0 +1,101 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const container = document.getElementById("post-js")
+console.log(container)
+
+let htmlContent = ""
+
+posts.forEach(function(posts){
+    htmlContent += `
+    <div class="container">
+
+         <div class="user-info">
+                <img class="profile-icon" src="${posts.avatar}">
+
+                <div class="name-loc">
+                        <p class="bld">${posts.name}</p>
+                        <p>${posts.location}</p>
+                 </div>
+
+        </div>
+
+        <img class="post" src="${posts.post}">
+
+        <div class="icons">
+             <button class="btn" id="btn-${posts.username}"><img class="icon heart" src="images/icon-heart.png"></button>
+             <button class="btn"><img class="icon comment" src="images/icon-comment.png"></button>
+            <button class="btn"><img class="icon comment" src="images/icon-dm.png"></button>
+        </div>
+
+        <div class="likes-cpt">
+             <p class="bld" id="${posts.username}">${posts.likes} likes</p>
+             <p><span class="bld">${posts.username}</span> ${posts.comment} </p>
+        </div>
+
+    </div>`
+})
+container.innerHTML = htmlContent
+
+
+console.log(posts[1].likes + 1)
+
+const vanBtn = document.getElementById("btn-vincey1853")
+const vanLikesSection = document.getElementById("vincey1853")
+let likeCountVan = posts[0].likes
+
+vanBtn.addEventListener("click", function(){
+    likeCountVan += 1
+    let vanLikes = `
+             <p class="bld" id="${posts.username}">${likeCountVan} likes</p>`
+    vanLikesSection.innerHTML = vanLikes;
+})
+
+const gusBtn = document.getElementById("btn-gus1819")
+const gusLikesSection = document.getElementById("gus1819")
+let likeCountGus = posts[1].likes
+
+gusBtn.addEventListener("click", function(){
+    likeCountGus += 1
+    let gusLikes = `
+             <p class="bld" id="${posts.username}">${likeCountGus} likes</p>`
+    gusLikesSection.innerHTML = gusLikes;
+})
+
+const jdBtn = document.getElementById("btn-jd1735")
+const jdLikesSection = document.getElementById("jd1735")
+let likeCountJd = posts[2].likes
+
+jdBtn.addEventListener("click", function(){
+    likeCountJd += 1
+    let jdLikes = `
+             <p class="bld" id="${posts.username}">${likeCountJd} likes</p>`
+    jdLikesSection.innerHTML = jdLikes;
+})
